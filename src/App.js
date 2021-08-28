5fba651bbed4010e0f6f8d788355bf8ccd0fe31f
Leonardo Moreira <leonardo@valeti.com>
2021-08-28 16:45:56 -0300

    setting routes file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+        <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/new_team">
+            <Users />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Users() {
+  return <h2>New Team</h2>;
+}
